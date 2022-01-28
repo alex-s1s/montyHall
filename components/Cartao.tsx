69b69cbcd468b3fe5347db7cf32870ded058da02
alex silva <alex-silva@MacBook-Pro-de-alex.local>
2022-01-28 15:56:01 -0300

    feat: create project monty hall

---
+++ components/Cartao.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/cartao.module.css";
+
+interface CartaoProps {
+  bgcolor?: string;
+  children?: any;
+}
+
+export default function Cartao(props) {
+  return (
+    <div
+      className={styles.cartao}
+      style={{
+        backgroundColor: props.bgcolor ?? "#fff",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+}
